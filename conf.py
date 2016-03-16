db0e335ce62d22180b969c0455d3f4b4276b69e9
C. Titus Brown <titus@idyll.org>
2016-03-16 13:40:25 +0100

    update suffixes

--- conf.py
+++ conf.py
@@ -35,7 +35,7 @@ extensions = ['sphinx.ext.mathjax', 'sphinxcontrib.images']
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
